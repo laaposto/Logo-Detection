13dffdb351fff3704cdfc8f6217a6bf15432943a
laaposto <laaposto@gmail.com>
2017-09-25 13:27:42 +0300

    Added WikiURL in table source

--- table/js/main.js
+++ table/js/main.js
@@ -9,7 +9,7 @@ $.ajax({
             for (var k = 0; k < json.logofiles[i].length; k++) {
                 images += '<img class="logo_img" src="' + json.logofiles[i][k] + '"/>';
             }
-            $('#table').append('<tr><td>' + images + '</td><td>' + json.logonames[i] + '</td></tr>');
+            $('#table').append('<tr><td>' + images + '</td><td><a href="' + json.logowikilinks[i] + '" target="_blank">' + json.logonames[i] + '</a></td></tr>');
         }
     },
     error: function () {
